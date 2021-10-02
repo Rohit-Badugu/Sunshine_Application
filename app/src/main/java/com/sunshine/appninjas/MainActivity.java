3f281b08fe58a207eb548f5df74e10c0157cfa46
Venkat Rebba <rebba498@gmail.com>
2021-10-01 18:15:17 -0700

    GPS permission taking

--- app/src/main/java/com/sunshine/appninjas/MainActivity.java
+++ app/src/main/java/com/sunshine/appninjas/MainActivity.java
@@ -1,14 +1,88 @@
 package com.sunshine.appninjas;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import androidx.core.content.ContextCompat;
+import android.util.Log;
+import android.Manifest;
+import android.widget.Toast;
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.EasyPermissions;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private final int REQUEST_LOCATION_PERMISSION = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        requestLocationPermission();
+
+        String current_location = get_location();
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        // Forward results to EasyPermissions
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+    }
+
+    @AfterPermissionGranted(REQUEST_LOCATION_PERMISSION)
+    public void requestLocationPermission() {
+        String[] perms = {Manifest.permission.ACCESS_FINE_LOCATION};
+        if(EasyPermissions.hasPermissions(this, perms)) {
+            Toast.makeText(this, "Permission already granted", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            EasyPermissions.requestPermissions(this, "Please grant the location permission", REQUEST_LOCATION_PERMISSION, perms);
+        }
+    }
+
+
+    public String get_location(){
+
+        String newloc = "0,0";
+
+        //Checking for location permissions
+        if (check_permission(1)) {
+            GPSTrack gps;
+            gps = new GPSTrack(MainActivity.this);
+            double latitude = gps.getLatitude();
+            double longitude = gps.getLongitude();
+
+            if (gps.canGetLocation()) {
+                if (latitude != 0 || longitude != 0) {
+                    System.out.println("New data" + latitude + "," + longitude);
+                    newloc = latitude+","+longitude;
+
+                } else {
+                    Log.i("New Updated Location:", "NULL");
+                }
+            }else{
+                System.out.println("GPS is not enabled!");
+            }
+        }
+
+        return newloc;
+    }
+
+
+    public boolean check_permission(int permission){
+        switch(permission){
+            case 1:
+                return ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+
+//            case 2:
+//                return ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
+        }
+        return false;
+    }
+
+
 }
